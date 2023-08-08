8b2614eb86dccf79a8908954f4af9ffd7483ab79
ivanvarella <ivanvarella@gmail.com>
2023-08-08 10:38:17 -0300

    Active Link with  NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 // 2- link with React Router
 import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="about">About</Link>
+      <NavLink
+        to="/"
+        //className={({ isActive }) => (isActive ? "esta-ativo" : "nao-ativo")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        //className={({ isActive }) => (isActive ? "esta-ativo" : "nao-ativo")}
+      >
+        About
+      </NavLink>
     </nav>
   );
 };
