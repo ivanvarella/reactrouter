9225bd7d2616956fb28ea91826de310989396ea4
ivanvarella <ivanvarella@gmail.com>
2023-08-08 10:30:06 -0300

    No match found route - 404

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>Sorry, this page does not exist</p>
+      <p>Please try again</p>
+    </div>
+  );
+};
+
+export default NotFound;
