c940aefda142985dcc56837ce0070d359adedad7
ivanvarella <ivanvarella@gmail.com>
2023-08-08 09:27:45 -0300

    Load indivudual product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,30 @@
-import { useParams  } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
-    // 4- Dynamic route
-    const { id } = useParams();
+  // 4- Dynamic route
+  const { id } = useParams();
+
+  //5- Individual product load
+  const url = "http://localhost:3004/products/" + id;
+  const { data: product, loading, error } = useFetch(url);
 
   return (
     <>
-        <p>ID do produto: {id}</p>
-    </>
-  )
-}
+      <p>ID do produto: {id}</p>
 
-export default Product
+      {error && <p>{error}</p>}
+
+      {loading && <p>Carregando...</p>}
+
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R${product.price}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Product;
